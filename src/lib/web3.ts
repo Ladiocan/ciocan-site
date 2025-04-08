1f2d7b26498d10d073e73a59c3539f02a2f23290
Ladislau Ciocan <ladislau.ciocan@turistintransilvania.com>
2025-04-08 15:36:54 +0300

    Fix wagmi config for v2.14

--- src/lib/web3.ts
+++ src/lib/web3.ts
@@ -1,13 +1,15 @@
-import { configureChains, createConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
-
-const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum],
-  [publicProvider()]
-);
+import { createConfig, fallback, http } from 'wagmi'
+import { injected } from 'wagmi/connectors'
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains'
 
 export const config = createConfig({
-  autoConnect: true,
-  publicClient,
-});
+  chains: [mainnet, polygon, optimism, arbitrum],
+  connectors: [injected()],
+  transports: {
+    [mainnet.id]: fallback([http()]),
+    [polygon.id]: fallback([http()]),
+    [optimism.id]: fallback([http()]),
+    [arbitrum.id]: fallback([http()]),
+  },
+  ssr: true,
+})
